8d1d98425513050179a741417fcc7a3d2c54d440
Passa <krsna@krsnas-Mac-Pro.local>
2015-04-04 02:56:35 -0700

    just added info cell

---
+++ NSA at ULM/infoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  infoTableViewCell.swift
+//  NSA at ULM
+//
+//  Created by krsna on 4/4/15.
+//  Copyright (c) 2015 Krnsa. All rights reserved.
+//
+
+import UIKit
+
+class infoTableViewCell: UITableViewCell {
+
+    
+     var name: UILabel = UILabel()
+     var phone: UILabel = UILabel()
+     var email: UILabel = UILabel()
+     var position: UILabel = UILabel()
+     var imgur: UIImageView!
+}
